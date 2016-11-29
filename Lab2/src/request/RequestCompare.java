2b33cd9f055ebb8cd1216e54e15ee76c5c788bf3
L-KID <lolmesx@gmail.com>
2016-11-29 21:03:56 +0100

    finish clock and request

---
+++ Lab2/src/request/RequestCompare.java
@@ -0,0 +1,16 @@
+package request;
+
+import java.util.Comparator;
+
+
+public class RequestCompare implements Comparator<Request>{
+	public int compare(Request r1, Request r2){
+		if(r1.timestamp.olderThan(r2.timestamp)){
+			return -1;
+		}
+		else{
+			return 1;
+		}
+	}
+
+}
